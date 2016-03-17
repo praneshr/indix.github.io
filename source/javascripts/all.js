22d2f0fe4452fa7fe4b0bb2fc82a9f56c5bd4393
Manoj <manojlds@gmail.com>
2016-03-17 16:02:20 +0530

    Add description, language, stars and forks

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,16 +1,15 @@
 $(function() {
-  var repos = {};
   $.get('https://api.github.com/search/repositories?per_page=20&page=1&q=user:ind9&sort=stars', function(data) {
     if(!data) return;
 
     data.items.forEach(function(repo) {
-      repos[repo.name] = {
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        language: repo.language,
-        stars: repo.stargazers_count,
-        forks: repo.forks
+      var repoContainer = $('#' + repo.name);
+      if(repoContainer.length > 0) {
+        var description = $('<p>').text(repo.description);
+        $('.panel-body', repoContainer).append(description);
+        $('.language', repoContainer).text(repo.language);
+        $('.stars', repoContainer).text(repo.stargazers_count);
+        $('.forks', repoContainer).text(repo.forks);
       }
     });
 
