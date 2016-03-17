89582523d47c69696c55ae19641eaaa5554f78d2
Manoj <manojlds@gmail.com>
2016-03-17 15:44:20 +0530

    Hookup projects

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
-// This is where it all goes :)
+$(function() {
+  var repos = {};
+  $.get('https://api.github.com/search/repositories?per_page=20&page=1&q=user:ind9&sort=stars', function(data) {
+    if(!data) return;
+
+    data.items.forEach(function(repo) {
+      repos[repo.name] = {
+        name: repo.name,
+        url: repo.html_url,
+        description: repo.description,
+        language: repo.language,
+        stars: repo.stargazers_count,
+        forks: repo.forks
+      }
+    });
+
+    console.log(repos);
+  });
+});
