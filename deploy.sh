268d4aea57346702df81bee3c133a05112f38121
Manoj <manojlds@gmail.com>
2016-03-18 12:38:19 +0530

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,16 @@ set -e
 
 DEPLOY_FOLDER="deploy/"
 
+bundle install
+bundle exec middleman build
+
 if [ ! -d "$DEPLOY_FOLDER" ]; then
   git clone -b master git@github.com:ind9/ind9.github.io $DEPLOY_FOLDER
 fi
 
-bundle install
-bundle exec middleman build
-cp -r build/ $DEPLOY_FOLDER
-
 cd $DEPLOY_FOLDER
+git pull origin master
+cp -r ../build/ .
 
 git add -A
 git commit -am "Site generated at $(date)"
