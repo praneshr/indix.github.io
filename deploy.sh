960a23903f9e4d415a1d7ae499c796d77fabe473
Manoj <manojlds@gmail.com>
2016-03-18 11:28:18 +0530

    Add simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+DEPLOY_FOLDER="deploy/"
+
+if [ ! -d "$DEPLOY_FOLDER" ]; then
+  git clone -b master git@github.com:ind9/ind9.github.io $DEPLOY_FOLDER
+fi
+
+bundle exec middleman build
+cp -r build/ $DEPLOY_FOLDER
+
+cd $DEPLOY_FOLDER
+
+git add -A
+git commit -am "Site generated at $(date)"
+git push origin master
