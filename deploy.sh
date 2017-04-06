2d9b5ee717c8161bfc6038d1d911814ec799b60f
Manoj <manojlds@gmail.com>
2017-04-06 19:19:43 +0530

    Add .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,5 @@
 
 set -e
 
-DEPLOY_FOLDER="deploy/"
-
 bundle install
 bundle exec middleman build --verbose
-
-if [ ! -d "$DEPLOY_FOLDER" ]; then
-  git clone -b master git@github.com:indix/indix.github.io $DEPLOY_FOLDER
-fi
-
-cd $DEPLOY_FOLDER
-git pull origin master
-cp -r ../build/* .
-
-git add -A
-git commit -am "Site generated at $(date)"
-git push origin master
